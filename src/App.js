4075d75ba37e436d26b5671b6a6ae941beecfe4c
Adarsh Thakur <th.adarsh029@gmail.com>
2025-04-12 12:19:46 +0530

    learning react

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Help</li>
+    </nav>
+    <div className="container">
+     <h1>Adarsh Thakur</h1> 
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nobis sed quis explicabo.
+      Itaque quibusdam, eum adipisci labore aliquam sint asperiores iure. Impedit ipsam veritatis architecto quibusdam magnam velit ad?</p>
+    </div>
+    </>
+    
+  
+  );
+}
+
+export default App;
