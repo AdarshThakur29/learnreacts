2551a4bd2c5736000baedaa47cedfc0ee86d9a36
AdarshThakur29 <th.adarsh029@gmail.com>
2025-04-18 06:08:06 +0530

    learning

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import About from './Components/About';
+import React, { useState } from 'react';
+import Alert from './Components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
+ 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Help</li>
-    </nav>
-    <div className="container">
-     <h1>Adarsh Thakur</h1> 
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nobis sed quis explicabo.
-      Itaque quibusdam, eum adipisci labore aliquam sint asperiores iure. Impedit ipsam veritatis architecto quibusdam magnam velit ad?</p>
-    </div>
-    </>
-    
-  
+    <Router>
+  <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+  <Alert alert={alert}/>
+  <div className="container my-3">
+    <Routes>
+      <Route path="/about" element={<About mode={mode} />} />
+      <Route path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />} />
+    </Routes>
+  </div>
+</Router>
+    </> 
   );
 }
 
